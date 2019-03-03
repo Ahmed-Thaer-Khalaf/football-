16097465b36ee756514cf2d9f2faff2f3b4d9bc0
wvn-sport <48158716+wvn-sport@users.noreply.github.com>
2019-03-03 13:50:22 +0300

    data football
    js

---
+++ namesController.js
@@ -0,0 +1,69 @@
+angular.module('wvn', []).controller('titles', function($scope) {
+    $scope.home = [
+        
+       { 
+         title:'  احمد البشير يحاول تقليد نوري المالكي اضحك حتى الموت '
+         
+         ,image:'file:///sdcard/Download/a/1.jpg'
+
+        ,video:'file:///storage/emulated/0/Download/wvn/3.mp4'
+
+        ,video:'1'
+
+        ,data:'2018'
+
+        ,id:'1'
+
+        ,user:'البشير شو'
+
+       ,img:'img.jpg'
+},
+
+   
+       { 
+         title:'  احمد البشير يحاول تقليد نوري المالكي اضحك حتى الموت '
+         
+         ,image:'file:///sdcard/Download/a/1.jpg'
+  
+        ,video:'2'
+
+        ,data:'2018'
+
+        ,id:'2'
+
+        ,user:'البشير شو'
+        
+        ,img:'img.jpg'
+
+},
+
+
+
+{ 
+         title:'  احمد البشير يحاول تقليد نوري المالكي اضحك حتى الموت '
+         
+         ,image:'file:///sdcard/Download/a/1.jpg'
+
+        ,video:'3'
+
+        ,data:'2018'
+
+        ,id:'3'
+
+        ,user:'البشير شو'
+
+        ,img:'img.jpg'
+
+},
+
+
+
+
+
+
+       
+
+
+    ];
+});
+
